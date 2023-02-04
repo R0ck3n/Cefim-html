3c5528142fe70955f10d584c8be6b70dc65a7bea
R0ck3n <r0ck3n@hotmail.com>
2023-02-04 13:01:48 +0100

    annulation

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,2 @@
-const link = document.querySelectorAll("a")
-
-link.forEach(el=>{
-    el.href=`${location.host}${location.pathname}`
-})
-
-
 console.log('Bonjour a tous');
 console.log("L'exercisse étant basé principalement sur l'HTML et le CSS j'ai préferé gérer le burger menu avec ces techs plutôt qu'en utilisant un event listener JS")
